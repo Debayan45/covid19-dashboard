68d76b7bd79649e68c3b7f3010deaacf0f794be4
Debayan Chaudhuri <debayanchaudhury45@gmail.com>
2025-07-10 00:47:47 +0530

    Initial commit - COVID-19 Dashboard project

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="COVID-19 Global Impact Dashboard", layout="wide")
+
+st.title("🌍 COVID-19 Global Impact Dashboard")
+st.markdown("Welcome! This is a simple live test to check if Streamlit is working.")
